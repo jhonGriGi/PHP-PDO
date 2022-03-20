1e921f4e2b50b6e09879308dd546333f34728062
Darwin Grisales <darwingrisalesg@gmail.com>
2022-03-20 13:36:29 -0500

    Initialize the folders and files of the project

---
+++ src/core/dbconfig.php
@@ -0,0 +1,12 @@
+<?php 
+require ('../../vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable('../../');
+$dotenv->load();
+
+define("DB_DRIVER", $_ENV['DDBB_DRIVER']);
+define("DB_HOST", $_ENV['DDBB_HOST']);
+define("DB_NAME", $_ENV['DDBB_NAME']);
+define("DB_USER", $_ENV['DDBB_USER']);
+define("DB_PASS", $_ENV['DDBB_PASSWORD'])
+?>
